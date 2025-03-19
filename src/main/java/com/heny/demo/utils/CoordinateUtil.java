acb27a3404e7c9305fff8a471329759edd8fbc77
baoziss <505628125@qq.com>
2025-03-19 18:53:43 +0800

    1

--- src/main/java/com/heny/demo/utils/CoordinateUtil.java
+++ src/main/java/com/heny/demo/utils/CoordinateUtil.java
@@ -1,58 +1,46 @@
 package com.heny.demo.utils;
 
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import lombok.extern.log4j.Log4j2;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.referencing.CRS;
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.io.WKTReader;
 import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.crs.CRSAuthorityFactory;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
 
 @Log4j2
 public class CoordinateUtil {
-    public static Geometry coordinateTransform(Geometry sourceGeometry, int targetSrid){
-        if (sourceGeometry == null || sourceGeometry.getSRID() == 0 || targetSrid == 0){
-            return null;
-        }
+    /**
+     * 二维坐标转换
+     *
+     * @param x          源X坐标
+     * @param y          源Y坐标
+     * @param srcEPSG    源坐标系EPSG码
+     * @param targetEPSG 目标坐标系EPSG码
+     * @return 转换后坐标对象
+     */
+    public static Coordinate convert2D(double x, double y, int srcEPSG, int targetEPSG) {
         try {
-            CRSAuthorityFactory factory = CRS.getAuthorityFactory(true);
-            CoordinateReferenceSystem source = factory.createCoordinateReferenceSystem("EPSG:" + sourceGeometry.getSRID());
-            CoordinateReferenceSystem target = factory.createCoordinateReferenceSystem("EPSG:" + targetSrid);
-            MathTransform transform = CRS.findMathTransform(source, target,true);
-            Geometry res = JTS.transform(sourceGeometry, transform);
-            if (res != null){
-                res.setSRID(targetSrid);
-            }
-            return res;
-        }catch (FactoryException | TransformException e){
-            e.printStackTrace();
+            CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:" + srcEPSG, true);
+            CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:" + targetEPSG, true);
+            MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS, true);
+            return JTS.transform(new Coordinate(x, y), new Coordinate(), transform);
+        } catch (FactoryException | TransformException e) {
+            log.error("坐标转换失败: {}", e.getMessage());
+            throw new RuntimeException("Coordinate transform error", e);
         }
-        return null;
-    }
-
-    public static Geometry createGeometry(String wkt,int srid){
-        if (StringUtils.isEmpty(wkt)){
-            return null;
-        }
-        try {
-            WKTReader reader = new WKTReader();
-            Geometry geometry = reader.read(wkt);
-            geometry.setSRID(srid);
-            return geometry;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public static void main(String[] args) {
-        String wkt = "POINT (106.61 25.32)";
-        Geometry source = createGeometry(wkt,4490);
-        Geometry res = coordinateTransform(source, 4524);
-        int x = 1;
+        Coordinate src = new Coordinate(116.3975, 39.9087);
+
+        // 执行转换
+        Coordinate result = CoordinateUtil.convert2D(
+                src.x, src.y,
+                4326,  // WGS84
+                3857   // Web墨卡托
+        );
     }
 }
